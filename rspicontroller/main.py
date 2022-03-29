c461a5e8164828cfefe485b3030c6b83d25fa988
anders-wiggers <contact@anrs.dk>
2022-03-29 14:49:48 +0200

    funny main

--- rspicontroller/main.py
+++ rspicontroller/main.py
@@ -1,9 +1,25 @@
 
 from controller import servo_interface 
-from controller import stubServo
+from controller import rspiServo
 
-command = servo_interface.Servo(stubServo.StubServo())
-command.rotate(0)
-command.rotate(180)
-command.rotate(0)
-command.clearnUp()
+command = servo_interface.Servo(rspiServo.TowerPro(11))
+command1 = servo_interface.Servo(rspiServo.TowerPro(13))
+
+servos = []
+servos.append(command)
+servos.append(command1)
+
+for servo in servos:
+    servo.rotate(0)
+
+for servo in servos:
+    servo.rotate(180)
+
+for servo in servos:
+    servo.rotate(0)
+
+for servo in servos:
+    servo.rotate(180)
+
+for servo in servos:
+    servo.cleanUp()
