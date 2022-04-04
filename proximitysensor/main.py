4a670d3d6377cee6bc6d1f8c7de69cfea96033a0
Smytten <alex.aarhus@gmail.com>
2022-04-04 12:13:29 +0200

    changed CAP1188 CHANNEL to pin number

--- proximitysensor/main.py
+++ proximitysensor/main.py
@@ -14,7 +14,7 @@ cap = CAP1188_I2C(i2c)
 # cs = DigitalInOut(board.D5)
 # cap = CAP1188_SPI(spi, cs)
 
-previous_captured_pin = cap[1]
+previous_captured_pin = 0
 
 while True:
     for i in range(1, 9):
@@ -23,11 +23,11 @@ while True:
         if cap[i].value:
 
             # check if previous pin was touched before the current active pin
-            if previous_captured_pin < cap[i]:
+            if previous_captured_pin < i:
                 print("Sun has risen! Your planet is warming up")
 
-            if previous_captured_pin > cap[i]:
+            if previous_captured_pin > i:
                 print("Sun has set! Your planet is cooling down")
 
             #set latest captured pin to the activated pin
-            previous_captured_pin = cap[i]
+            previous_captured_pin = i
