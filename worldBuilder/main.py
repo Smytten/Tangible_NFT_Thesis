886eb8a25162eb0b8dfae34e2a14719f47fae07a
anders-wiggers <contact@anrs.dk>
2022-04-06 00:21:48 +0200

    added world skeleton to project

---
+++ worldBuilder/main.py
@@ -0,0 +1,20 @@
+class tile():
+    def __init__():
+        pass
+
+class world():
+
+    def __init__():
+        pass
+
+    def getFace():
+        pass
+
+    def turnWorldLft():
+        pass
+
+    def turnWorldRgt():
+        pass
+
+    def updateWorld():
+        pass
