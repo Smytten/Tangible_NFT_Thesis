afe36045fa79e622ef0fb4d912492cf9b4c2619e
Smytten <alex.aarhus@gmail.com>
2022-04-06 13:26:24 +0200

    sunrise class

--- proximitysensor/sunrise.py
+++ proximitysensor/sunrise.py
@@ -54,27 +54,30 @@ class sunController():
             pixels.show()
             time.sleep(wait)
 
+    def run():
+        while True:
+            # Comment this line out if you have RGBW/GRBW NeoPixels
+            pixels.fill((255, 0, 0))
+            # Uncomment this line if you have RGBW/GRBW NeoPixels
+            # pixels.fill((255, 0, 0, 0))
+            pixels.show()
+            time.sleep(1)
 
-    while True:
-        # Comment this line out if you have RGBW/GRBW NeoPixels
-        pixels.fill((255, 0, 0))
-        # Uncomment this line if you have RGBW/GRBW NeoPixels
-        # pixels.fill((255, 0, 0, 0))
-        pixels.show()
-        time.sleep(1)
+            # Comment this line out if you have RGBW/GRBW NeoPixels
+            pixels.fill((0, 255, 0))
+            # Uncomment this line if you have RGBW/GRBW NeoPixels
+            # pixels.fill((0, 255, 0, 0))
+            pixels.show()
+            time.sleep(1)
 
-        # Comment this line out if you have RGBW/GRBW NeoPixels
-        pixels.fill((0, 255, 0))
-        # Uncomment this line if you have RGBW/GRBW NeoPixels
-        # pixels.fill((0, 255, 0, 0))
-        pixels.show()
-        time.sleep(1)
+            # Comment this line out if you have RGBW/GRBW NeoPixels
+            pixels.fill((0, 0, 255))
+            # Uncomment this line if you have RGBW/GRBW NeoPixels
+            # pixels.fill((0, 0, 255, 0))
+            pixels.show()
+            time.sleep(1)
 
-        # Comment this line out if you have RGBW/GRBW NeoPixels
-        pixels.fill((0, 0, 255))
-        # Uncomment this line if you have RGBW/GRBW NeoPixels
-        # pixels.fill((0, 0, 255, 0))
-        pixels.show()
-        time.sleep(1)
+            rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
 
-        rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
+obj = sunController()
+obj.run()
