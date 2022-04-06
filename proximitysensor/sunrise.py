cbc3129e43711fd6cc84829b606fbfd8bc787ce0
Smytten <alex.aarhus@gmail.com>
2022-04-06 12:19:49 +0200

    strip instead of individual

--- proximitysensor/sunrise.py
+++ proximitysensor/sunrise.py
@@ -9,10 +9,10 @@ import neopixel
 
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
 # NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = board.D18
+pixel_pin = board.D10
 
 # The number of NeoPixels
-num_pixels = 30
+num_pixels = 13
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
