74e469d07ece14ea41939c643fc8ae2a9cf48625
Smytten <alex.aarhus@gmail.com>
2022-04-06 15:45:02 +0200

    TDD do over

--- proximitysensor/sunrise.py
+++ proximitysensor/sunrise.py
@@ -23,60 +23,19 @@ class sunController():
 
         self.pixels = neopixel.NeoPixel(
             self.pixel_pin, self.num_pixels, brightness=0.2, auto_write=False, pixel_order=self.ORDER)
-
-
-    def wheel(self, pos):
-        # Input a value 0 to 255 to get a color value.
-        # The colours are a transition r - g - b - back to r.
-        if pos < 0 or pos > 255:
-            r = g = b = 0
-        elif pos < 85:
-            r = int(pos * 3)
-            g = int(255 - pos * 3)
-            b = 0
-        elif pos < 170:
-            pos -= 85
-            r = int(255 - pos * 3)
-            g = 0
-            b = int(pos * 3)
-        else:
-            pos -= 170
-            r = 0
-            g = int(pos * 3)
-            b = int(255 - pos * 3)
-        return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
-
-
-    def rainbow_cycle(self, wait):
-        for j in range(255):
-            for i in range(num_pixels):
-                pixel_index = (i * 256 // num_pixels) + j
-                pixels[i] = wheel(pixel_index & 255)
-            pixels.show()
-            time.sleep(wait)
     
     def sunrise(self):
         print("sunrise")
         
         while True:
             for i in range(self.num_pixels):
-                self.pixels[i] = (75,255,63)
+                self.pixels[i] = (246,87,38)
                 self.pixels.show()
-                time.sleep(1)
+                time.sleep(0.5)
                 
             self.pixels.fill((0,0,0))
             self.pixels.show()
             
-        #while True:
-        #    #self.pixels.fill((255, 234, 209))
-        #    self.pixels.fill((255, 0, 0))
-        #    self.pixels.show()
-        #    time.sleep(5)
-        #    #self.pixels.fill((255, 141, 9))
-        #    self.pixels.fill((0, 255, 0))
-        #    self.pixels.show()
-        #    time.sleep(5)
-        
         
     def sunset(self):
         print("sunset")
