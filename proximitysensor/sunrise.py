0bdc965866562b6e8e73737275a626f73535b282
Smytten <alex.aarhus@gmail.com>
2022-04-06 13:31:00 +0200

    sunrise class

--- proximitysensor/sunrise.py
+++ proximitysensor/sunrise.py
@@ -57,27 +57,27 @@ class sunController():
     def run(self):
         while True:
             # Comment this line out if you have RGBW/GRBW NeoPixels
-            pixels.fill((255, 0, 0))
+            self.pixels.fill((255, 0, 0))
             # Uncomment this line if you have RGBW/GRBW NeoPixels
             # pixels.fill((255, 0, 0, 0))
-            pixels.show()
+            self.pixels.show()
             time.sleep(1)
 
             # Comment this line out if you have RGBW/GRBW NeoPixels
-            pixels.fill((0, 255, 0))
+            self.pixels.fill((0, 255, 0))
             # Uncomment this line if you have RGBW/GRBW NeoPixels
             # pixels.fill((0, 255, 0, 0))
-            pixels.show()
+            self.pixels.show()
             time.sleep(1)
 
             # Comment this line out if you have RGBW/GRBW NeoPixels
-            pixels.fill((0, 0, 255))
+            self.pixels.fill((0, 0, 255))
             # Uncomment this line if you have RGBW/GRBW NeoPixels
             # pixels.fill((0, 0, 255, 0))
-            pixels.show()
+            self.pixels.show()
             time.sleep(1)
 
-            rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
+            self.rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
 
 obj = sunController()
 obj.run()
