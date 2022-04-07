24e7eb54af7626750d75a2ee20e340b53c52d268
Smytten <alex.aarhus@gmail.com>
2022-04-07 11:42:36 +0200

    keyframes

--- proximitysensor/sunrise.py
+++ proximitysensor/sunrise.py
@@ -23,30 +23,40 @@ class sunController():
 
         self.pixels = neopixel.NeoPixel(
             self.pixel_pin, self.num_pixels, brightness=0.2, auto_write=False, pixel_order=self.ORDER)
-    
+
+        self.KEYFRAMES_RISING = [20, 21, 22, 24, 26, 28, 31, 34, 38, 41, 45, 50, 55, 60, 66, 73, 80, 87, 95,
+                                 103, 112, 121, 131, 141, 151, 161, 172, 182, 192, 202, 211, 220, 228, 236,
+                                 242, 247, 251, 254, 255]
+        
+        self.KEYFRAMES_SETTING = [254, 251, 247, 242, 236, 228, 220, 211, 202, 192, 182, 172, 161, 151, 141,
+                                  131, 121, 112, 103, 95, 87, 80, 73, 66, 60, 55, 50, 45, 41, 38, 34, 31, 28,
+                                  26, 24, 22, 21, 20,
+                                  20, 20, 20, 20, 20, 20, 20, 20, 20, 20]
+
     def sunrise(self):
         print("sunrise")
-        
+
         while True:
             for i in range(self.num_pixels):
                 if i > 3:
-                    self.pixels[i-3] = (255,183,38)
+                    for frame in self.KEYFRAMES_RISING:
+                        self.pixels[i-3] = (frame, 183, 38)
                 if i > 5:
-                    self.pixels[i-5] = (0,0,0)
-                if i < self.num_pixels:
-                    self.pixels[i+1] = (255,183,38)
-                
-                for j in range (135,30,-1):
-                    self.pixels[i] = (255,j,0)
+                    for frame in self.KEYFRAMES_SETTING:
+                        self.pixels[i-5] = (frame, 183, 38)
+                if i < self.num_pixels-1:
+                    self.pixels[i+1] = (255, 183, 38)
+
+                for j in range(135, 30, -1):
+                    self.pixels[i] = (255, j, 0)
                     self.pixels.show()
-                    #time.sleep(0.5)
-                    
+                    # time.sleep(0.5)
+
                 time.sleep(0.5)
-                
-            self.pixels.fill((0,0,0))
+
+            self.pixels.fill((0, 0, 0))
             self.pixels.show()
-            
-        
+
     def sunset(self):
         print("sunset")
 
