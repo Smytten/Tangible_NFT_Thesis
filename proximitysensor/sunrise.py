df75f1d51fbda88f8a9f207c06c4b1a028a633fc
Smytten <alex.aarhus@gmail.com>
2022-04-08 13:23:35 +0200

    sunset test

--- proximitysensor/sunrise.py
+++ proximitysensor/sunrise.py
@@ -108,12 +108,12 @@ class sunController():
                 self.pixels.show()
 
             # Move accent above down
-            if i < 8:
+            if i < 9:
                 for j in self.ACCENT_FRAMES:
                     self.pixels[i-1] = (j)
                     self.pixels.show()
             #turn off after accent
-            if i < 6:
+            if i < 7:
                 for j in self.ACCENT_FRAMES:
                     self.pixels[i-6] = (0,0,0)
                     self.pixels.show()
