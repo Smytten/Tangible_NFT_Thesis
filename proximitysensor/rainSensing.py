6272f22595aed8dc977dbb1dd78fd3afd3e760b7
anders-wiggers <contact@anrs.dk>
2022-05-12 10:30:41 +0200

    added rainsensing

---
+++ proximitysensor/rainSensing.py
@@ -0,0 +1,25 @@
+import board
+import time
+from adafruit_cap1188.i2c import CAP1188_I2C
+
+if __name__ == "__main__":
+    import CONST as c
+else:
+    import proximitysensor.CONST as c
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+cap = CAP1188_I2C(i2c,c.RAINSENSOR_ADDRESS)
+
+# SPI setup
+# from digitalio import DigitalInOut, Direction
+# from adafruit_cap1188.spi import CAP1188_SPI
+# spi = board.SPI()
+# cs = DigitalInOut(board.D5)
+# cap = CAP1188_SPI(spi, cs)
+
+while True:
+    for i in range(1, 6):
+        if cap[i].value:
+            print("Pin {} touched!".format(i))
+    time.sleep(0.001)
+
