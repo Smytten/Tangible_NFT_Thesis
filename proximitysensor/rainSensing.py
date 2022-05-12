99ee2a0b6eb2cc0d3cce4fed66d82ff2624c614f
anders-wiggers <contact@anrs.dk>
2022-05-12 11:14:29 +0200

    stabilization

--- proximitysensor/rainSensing.py
+++ proximitysensor/rainSensing.py
@@ -17,14 +17,30 @@ cap = CAP1188_I2C(i2c,c.RAINSENSOR_ADDRESS)
 # cs = DigitalInOut(board.D5)
 # cap = CAP1188_SPI(spi, cs)
 
+activations = []
+prevActive = []
+
 timerArr = []
 curTime = time.time()
 for i in range(6):
     timerArr.append(curTime)
+    activations.append(0)
+    prevActive.append(False)
 
 while True:
     for i in range(1, 7):
         if cap[i].value and timerArr[i-1] < time.time():
-            print("Pin {} touched!".format(i))
-            timerArr[i-1] = time.time() + 60
+            if prevActive[i-1]:
+                activations[i-1] += 1
+                if activations[i-1] == 200:
+                    print("Pin {} touched!".format(i))
+                    timerArr[i-1] = time.time() + 5
+
+            else:
+                prevActive[i-1] = True
+
+
+        else:
+            prevActive[i-1] = False
+            activations[i-1] = 0
 
