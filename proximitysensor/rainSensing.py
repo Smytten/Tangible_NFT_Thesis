eb05916db3a5c6801466d2d59b0373d2888c68fc
anders-wiggers <contact@anrs.dk>
2022-05-12 11:22:02 +0200

    released mechanism

--- proximitysensor/rainSensing.py
+++ proximitysensor/rainSensing.py
@@ -19,6 +19,7 @@ cap = CAP1188_I2C(i2c,c.RAINSENSOR_ADDRESS)
 
 activations = []
 prevActive = []
+released = []
 
 timerArr = []
 curTime = time.time()
@@ -26,15 +27,17 @@ for i in range(6):
     timerArr.append(curTime)
     activations.append(0)
     prevActive.append(False)
+    released.append(True)
 
 while True:
     for i in range(1, 7):
         if cap[i].value and timerArr[i-1] < time.time():
             if prevActive[i-1]:
                 activations[i-1] += 1
-                if activations[i-1] == c.ACTIVATION_TIME:
+                if activations[i-1] == c.ACTIVATION_TIME and released[i-1]:
                     print("Pin {} touched!".format(i))
                     timerArr[i-1] = time.time() + c.WAIT_DURATION
+                    released[i-1] = False
 
             else:
                 prevActive[i-1] = True
@@ -43,4 +46,5 @@ while True:
         else:
             prevActive[i-1] = False
             activations[i-1] = 0
+            released[i-1] = True
 
