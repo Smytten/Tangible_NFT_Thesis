3c44cbb006b2e479cf7902e12d2d87ab2e326703
anders-wiggers <contact@anrs.dk>
2022-05-13 14:08:41 +0200

    new actutate script

--- rspicontroller/actuate.py
+++ rspicontroller/actuate.py
@@ -2,12 +2,16 @@ from servo_controller import servo_interface
 from servo_controller import rspiServo
 from servo_controller import multipleServoController
 
-command = servo_interface.Servo(rspiServo.TowerPro(11))
+command0 = servo_interface.Servo(rspiServo.TowerPro(11))
 command1 = servo_interface.Servo(rspiServo.TowerPro(13))
+command2 = servo_interface.Servo(rspiServo.TowerPro(15))
+command3 = servo_interface.Servo(rspiServo.TowerPro(16))
+command4 = servo_interface.Servo(rspiServo.TowerPro(18))
+command5 = servo_interface.Servo(rspiServo.TowerPro(22))
 
-servos = [command, command1]
+servos = [command0, command1, command2, command3, command4, command5]
 
-mc = multipleServoController.MultiController([command,command1])
+mc = multipleServoController.MultiController(servos)
 
 try:
     while True:
