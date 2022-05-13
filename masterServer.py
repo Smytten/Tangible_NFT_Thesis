eee7e61be7b785b280e01da5f462d5393040c0f2
anders-wiggers <contact@anrs.dk>
2022-05-13 10:38:17 +0200

    added rainprocess as daemon to kill it on exit

--- masterServer.py
+++ masterServer.py
@@ -13,13 +13,12 @@ f = open('worldBuilder/world.json')
 jf = json.load(f)
 world.importJSON(jf)
 
-thread = threading.Thread(target=rainSensing.activateSensor,args=(world.rainfall,))
-try:
-    thread.start() 
-except (KeyboardInterrupt, SystemExit):
-    thread.join(timeout=0)
-    sys.exit()
-# rainSensing.activateSensor(world.rainfall)
+# Starting Rain detecting script
+rainProcess = threading.Thread(target=rainSensing.activateSensor,args=(world.rainfall,))
+rainProcess.daemon = True
+rainProcess.start() 
+
+# Main process of the world
 while(True):
     time.sleep(10)
     print(world.exportJSON())
