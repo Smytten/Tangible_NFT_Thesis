dcc28430de7f819865b66368a4f43881d97cb251
anders-wiggers <contact@anrs.dk>
2022-05-13 10:29:22 +0200

    manual do stuff with x servoes

--- rspicontroller/mainrspi.py
+++ rspicontroller/mainrspi.py
@@ -5,7 +5,17 @@ from servo_controller import multipleServoController
 command = servo_interface.Servo(rspiServo.TowerPro(11))
 command1 = servo_interface.Servo(rspiServo.TowerPro(13))
 
+servos = [command, command1]
+
 mc = multipleServoController.MultiController([command,command1])
+
+try:
+    while True:
+        rotations = input(f'Enter inputs for {len(servos)} servos seperated with space') 
+        pattern = rotations.split()
+        mc.actuatePattern(pattern)
+finally:
+    mc.cleanup()
 
 rotationPattern1 = [180,0]
 rotationPattern2 = [0,180]
