dab1b0c83f947d34d25d3736d63248cfb39f6311
anders-wiggers <contact@anrs.dk>
2022-05-13 10:01:44 +0200

    ss

--- masterServer.py
+++ masterServer.py
@@ -12,7 +12,8 @@ f = open('worldBuilder/world.json')
 jf = json.load(f)
 world.importJSON(jf)
 
-thread = threading.Thread(target=rainSensing.activateSensor,args=world.rainfall)
+thread = threading.Thread(target=rainSensing.activateSensor,args=(world.rainfall,))
+thread.start()
 # rainSensing.activateSensor(world.rainfall)
 while(True):
     time.sleep(10)
