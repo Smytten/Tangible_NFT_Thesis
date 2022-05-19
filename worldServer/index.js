2a07e1ad9f6d1f462ce5b17f7d2d2a054a46fe96
anders-wiggers <contact@anrs.dk>
2022-05-19 14:08:41 +0200

    added cur user

--- worldServer/index.js
+++ worldServer/index.js
@@ -1,32 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 9999
-const fs = require(
-    'fs'
-)
+const express = require('express');
+const app = express();
+const port = 9999;
+const fs = require('fs');
 
-app.use(express.json())
+app.use(express.json());
+
+let user = 'test';
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+	res.send('Hello World!');
+});
+
+app.get('/user', (req, res, next) => {
+	res.send(user);
+});
+
+app.post('/user', (req, res, next) => {
+	user = req.query['id'];
+	console.log(user);
+	res.send('ok');
+});
 
 app.get('/:world', (req, res, next) => {
-    let jsonToFetch = 'worlds/' + req.params.world + '.json'
-    let rawdata = fs.readFileSync(jsonToFetch);
-    let student = JSON.parse(rawdata);
-    res.send(student)
-    next()
-})
+	let jsonToFetch = 'worlds/' + req.params.world + '.json';
+	let rawdata = fs.readFileSync(jsonToFetch);
+	let student = JSON.parse(rawdata);
+	res.send(student);
+	next();
+});
 
 app.post('/:world', (req, res, next) => {
-    console.log(req.body)
-    let world = JSON.stringify(req.body)
-    fs.writeFileSync('worlds/' + req.params.world + '.json', world)
-    res.send("Saved World")  // <==== req.body will be a parsed JSON object
-
-})
+	console.log(req.body);
+	let world = JSON.stringify(req.body);
+	fs.writeFileSync('worlds/' + req.params.world + '.json', world);
+	res.send('Saved World'); // <==== req.body will be a parsed JSON object
+});
 
 app.listen(port, () => {
-  console.log(` ${port}`)
-})
+	console.log(` ${port}`);
+});
